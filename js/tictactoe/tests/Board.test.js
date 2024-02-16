7fe7827a1fab7d15979fc6acf7def0ed90f2629d
isaac.pringle <isaac.pringle.1@spaceforce.mil>
2024-02-16 15:15:35 -0700

    test: add player and cpu tests

---
+++ js/tictactoe/tests/Board.test.js
@@ -0,0 +1,9 @@
+//const Board = require('../src').Board;
+
+test('draws an x when the player clicks a square', () => {
+    expect(1).toBe(1);
+});
+
+test('draws an o when the cpu clicks a square', () => {
+    expect(1).toBe(1);
+});
